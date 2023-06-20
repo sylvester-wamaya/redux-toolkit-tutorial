ff310925e7623add07affb20aed1955f1e7f0e60
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-06-20 20:46:12 +0300

    Add modal component

--- starter/src/App.js
+++ starter/src/App.js
@@ -3,6 +3,7 @@ import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
 import { useSelector, useDispatch } from "react-redux";
 import { cartTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const dispatch = useDispatch()
@@ -12,6 +13,7 @@ function App() {
   },[cartItems])
  return (
   <main>
+    <Modal/>
    <Navbar/>
    <CartContainer/>
    </main>
---
+++ starter/src/components/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <aside className='modal-container'>
+        <div className='modal'>
+            <h4>remove all items from your shopping cart?</h4>
+            <button type='button' className='btn confirm-btn'>confirm</button>
+            <button type='button' className='btn clear-btn'>cancel</button>
+        </div>
+        </aside>
+
+  )
+}
+
+export default Modal
